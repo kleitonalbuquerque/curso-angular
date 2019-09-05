b3777e65d43716fda3358133164d1eeb138ec220
kleitonalbuquerque <kleiton2102@gmail.com>
2019-09-05 10:35:57 -0300

    Configurando o arquivo tsconfig.json para separação dos arquivos js | Tipando variáveis em TS

---
+++ typescript/app.ts
@@ -0,0 +1,16 @@
+// Declaração de variáveis
+let message: string = "Help me, Obi-Wan Kenobi. You're my only hope!"
+console.log(message)
+
+let episode: number = 4
+console.log('This is episode ' + 4)
+episode = episode + 1
+console.log('Next episode is ' + episode)
+
+let favoriteDroid: string
+favoriteDroid = 'BB-8'
+console.log('My favorite droid is ' + favoriteDroid)
+
+let films: Array<string>
+films = ['Ameaça Fantasma', ' Ataque dos Clones', ' O Retorno de Jedi']
+console.log('Trilogia Star Wars: ' + films)
---
+++ typescript/dist/app.js
@@ -0,0 +1,13 @@
+// Declaração de variáveis
+var message = "Help me, Obi-Wan Kenobi. You're my only hope!";
+console.log(message);
+var episode = 4;
+console.log('This is episode ' + 4);
+episode = episode + 1;
+console.log('Next episode is ' + episode);
+var favoriteDroid;
+favoriteDroid = 'BB-8';
+console.log('My favorite droid is ' + favoriteDroid);
+var films;
+films = ['Ameaça Fantasma', ' Ataque dos Clones', ' O Retorno de Jedi'];
+console.log('Trilogia Star Wars: ' + films);
